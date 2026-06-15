453e37583de308abb390d1ca841748461cb58105
Emilio <balcazaemilio1@gmail.com>
2026-06-15 01:07:48 -0300

    agrego carrusel

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.addEventListener("click", (e) => {
-    console.log(e.target);
-});
+
 
 
 let btnRecargaCSS = document.getElementById("recargaCSS");
@@ -12,50 +10,121 @@ function fRecargarCSS(){
 }
 
 
+let btnCargarJSON = document.getElementById("cargarJSON");
+btnCargarJSON.addEventListener("click", cargarJSON);
 
 
+//Para cargar la infromacion a la ventana grande:
 
-let cards =
-    document.querySelectorAll(".miniCardProducto");
+let productos=[];
 
-console.log(cards);
 
+function encontrarObjetivoCard() {
+    
+let cards = document.querySelectorAll(".miniCardProducto");
+
+let idAux;
+
+let carouselContenedor = document.getElementById("carouselContenedor")
 
 
 cards.forEach(card => {
 
     card.addEventListener("click", () => {
 
-        console.log("Click en card");
+        // console.log("Click en card");
+        // console.log(card.id);
+        // idAux = parseInt(card.id);
+        // console.log(idAux);
+        // console.log(typeof(idAux));
 
 
-        // Obtener información de la card
-    let imagen = card.querySelector("img").src;
+            let idProducto = Number(card.id);
 
-    let titulo =
-        card.querySelector(".titulo").textContent;
+            let producto = productos.find(
+                p => p.id === idProducto
+            );
 
-    let descripcion =
-        card.querySelector(".descripcion").textContent;
-
-    // Pasar info a la card grande
-    document.getElementById("imgGrande").src =
-        imagen;
-
-    document.getElementById("tituloGrande").textContent =
-        titulo;
-
-    document.getElementById("descripcionGrande").textContent =
-        descripcion;
-
-
-
+            console.log(idProducto);
+   
+        cargarCardGrande(producto);
 
     });
 
 });
 
 
+ 
+}
+
+
+
+
+async function cargarCardGrande(producto){
+       
+       
+       try {
+           
+           let carouselContenedor = document.getElementById("carouselContenedor");
+
+   carouselContenedor.innerHTML="";
+   let carouselIndcators = document.getElementById("carouselIndcators");
+   carouselIndcators.innerHTML="";
+        let contador=0;
+
+        let tituloGrande = document.getElementById("tituloGrande");
+        tituloGrande.innerHTML = producto.titulo;
+
+        let descripcionGrande = document.getElementById("descripcionGrande");
+        descripcionGrande.innerHTML = producto.descripcion;
+
+    
+         producto.imagenes.forEach(element => {
+
+        
+           
+                     let carouselItem = document.createElement("div");
+                    carouselItem.classList.add("carousel-item");
+                    if(contador==0){
+                    carouselItem.classList.add("active");
+                        
+                    }
+
+                    let imgCarousel = document.createElement("img");
+                    imgCarousel.setAttribute("src", element);
+                    imgCarousel.classList.add("d-block");
+                    imgCarousel.classList.add("w-100");
+
+                    carouselItem.appendChild(imgCarousel);
+                    carouselContenedor.appendChild(carouselItem);
+
+                    let botonSlide = document.createElement("button");
+                    botonSlide.setAttribute("type","button");
+                    botonSlide.setAttribute("data-bs-target","#carouselExampleIndicators");
+                    botonSlide.setAttribute("data-bs-slide-to",contador);
+                    botonSlide.setAttribute("aria-label",`Slide ${contador+1}`);
+                                
+                    if(contador==0){
+                       
+                         botonSlide.classList.add("active");
+                         botonSlide.setAttribute("aria-current","true");
+                         botonSlide.setAttribute("aria-label",`Slide 1`);
+                    
+                         
+                    }
+                //    console.log(contador);
+                    carouselIndcators.appendChild(botonSlide);
+                    contador++;
+          
+            
+            
+         });
+    } catch (error) {
+        console.error(error);
+    }
+
+
+}
 
 
 
@@ -63,37 +132,109 @@ cards.forEach(card => {
 
 
 
+//para llenar las minicard
 
 
-/*
-let card = document.querySelector(".miniCardProducto");
-let imgCard = document.querySelector(".miniCardProductoImg");
-let descripcionCard = document.querySelector(".miniCardProductoDescripcion");
+
+async function cargarJSON() {
+  try {
+    const response = await fetch("./productos2026.json");
+    const data = await response.json();
+
+   // console.log(data);
+    let contenedorCards = document.getElementById("contenidoVistaChica");
+
+    data.productos.forEach(element => {
+     //   console.log(element);
+
+        let divMiniCard = document.createElement("div");
+        divMiniCard.classList.add("container");
+        divMiniCard.classList.add("col-sm-6");
+        divMiniCard.classList.add("col-md-6");
+        divMiniCard.classList.add("col-lg-4");
+        divMiniCard.classList.add("col-xl-4");
+        divMiniCard.classList.add("miniCardProducto");
+        divMiniCard.setAttribute("id",element.id);
+     
+        let divMiniCardProductoImg = document.createElement("div");
+        divMiniCardProductoImg.classList.add("miniCardProductoImg");
+        divMiniCardProductoImg.classList.add("col");
+
+        let divMiniCardProductoDescripcion = document.createElement("div");
+        divMiniCardProductoDescripcion.classList.add("miniCardProductoDescripcion");
+        divMiniCardProductoDescripcion.classList.add("col");
 
 
-card.addEventListener("click", () => {
+         let etiquetaImg = document.createElement("img");
+        let h5 = document.createElement("h5");
+        h5.classList.add("titulo");
 
-    // Obtener información de la card
-    let imagen = imgCard.querySelector("img").src;
 
-    let titulo =
-        descripcionCard.querySelector(".titulo").textContent;
+        let pDescripcion = document.createElement("p");
+        pDescripcion.setAttribute("id","pDescripcionChica");
+        pDescripcion.classList.add("descripcion");
 
-    let descripcion =
-        descripcionCard.querySelector(".descripcion").textContent;
 
-    // Pasar info a la card grande
-    document.getElementById("imgGrande").src =
-        imagen;
+        let pCategorias = document.createElement("p");
 
-    document.getElementById("tituloGrande").textContent =
-        titulo;
+         etiquetaImg.setAttribute("src", element.imagen1);
+    divMiniCardProductoImg.appendChild(etiquetaImg);
 
-    document.getElementById("descripcionGrande").textContent =
-        descripcion;
+    
+        
+        h5.innerHTML = element.titulo;
 
-    console.log("click");
+        pDescripcion.innerHTML = element.descripcion;
+
+        element.categorias.forEach(element => {
+            pCategorias.textContent += element + " ";
+        });
+
+
+        
+        divMiniCardProductoDescripcion.appendChild(h5);
+        divMiniCardProductoDescripcion.appendChild(pDescripcion);
+        divMiniCardProductoDescripcion.appendChild(pCategorias);
+
+
+        divMiniCard.appendChild(divMiniCardProductoImg);
+        divMiniCard.appendChild(divMiniCardProductoDescripcion);
+        
+        contenedorCards.appendChild(divMiniCard);
+
+    });
+
+
+
+    productos = data.productos;
+  encontrarObjetivoCard();
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+document.addEventListener("click", (e) => {
+    console.log(e.target);
+
 
 });
 
-*/
+
+
+
+
+//funcion inicial
+async function init() {
+  
+  await cargarJSON();
+ 
+
+  
+
+}
+
+init();
+
+
